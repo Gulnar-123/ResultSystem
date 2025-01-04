a52912e7dbfed5e406a1cfc5fc5dd656825b9c49
Gulnar-123 <gulnar6816@gmail.com>
2025-01-04 11:05:18 +0530

    first commit

---
+++ ResultSystem/src/main/java/com/example/ResultSystem/ResultSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResultSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultSystemApplication.class, args);
+	}
+
+}
